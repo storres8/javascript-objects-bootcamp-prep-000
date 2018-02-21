c6cbbbcc93b57b9a01778550915bad2bd7ae297f
Steven Torres <stevetorres7562@gmail.com>
2018-02-21 21:26:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,11 +4,11 @@ var playlist = {
 }; 
 
 function updatePlaylist(playlist,artist,song) {
-  playlist[artist] = song
+  playlist.artist = song
   return playlist;
 }
   
-function removeFromPlaylist(playlist,artistName){
-  delete playlist.artistName; 
+function removeFromPlaylist(playlist,artist){
+  delete playlist.artist 
   return playlist;
 } 
